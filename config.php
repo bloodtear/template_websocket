d3e1ccf5954cc2563304ae39de50420c29d58d68
bloodtear <xiaoyu1120520@126.com>
2018-01-31 17:06:13 +0800

    修改WSS_APP的错误

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ defined('WS_ROOT') or define('WS_ROOT', dirname(__FILE__) . "");
 defined('WS_APP') or define('WS_APP', WS_ROOT."/" . WS_APP . "/");
 
 
-defined('WSS_CONFIG') or define('WSS_CONFIG', "/var/www/" . APP . "_ws_config.php");
+defined('WSS_CONFIG') or define('WSS_CONFIG', "/var/www/" . WS_APP . "_ws_config.php");
 if (file_exists(WSS_CONFIG)) {
   include_once(WSS_CONFIG);
 }
