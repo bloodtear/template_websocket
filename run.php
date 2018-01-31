8a9337df7505a782ebb1c078cf4b524e5e334c4a
bloodtear <xiaoyu1120520@126.com>
2018-01-31 12:25:01 +0800

    修改ws的config和log

--- run.php
+++ run.php
@@ -1,9 +1,13 @@
 <?php
 
+// ****
+// config 信息不独立也无所谓，因为这个东西不需要链接到var/www内
+// 考虑到ws的使用情况，不作route处理，当div标签用，哪需要就贴到哪
+// Logging也独立出来，不再调用framework内，直接内部存留一份
 
 include_once("config.php");
 
-include_once("../framework/config.php");
+include_once("Logging.php");
 
 
 
